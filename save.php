abb67137d2ecbe195c5b232db2063c3fed8c5c7d
Daizy Bedi <daizybedi01june@gmail.com>
2025-04-13 12:14:27 +0530

    Save php added

---
+++ save.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $aadhar = $_POST['aadhar'];
+
+    $file = fopen("voters.csv", "a");
+    fputcsv($file, [$name, $city, $state, $aadhar], ",", '"', "\\");
+    fclose($file);
+
+    header("Location: authenticate.html");
+    exit();
+}
+?>
